f63bbba6705eb483450da5b3e9f359cd37422bcd
Konstantin <summerparadise938@gmail.com>
2020-11-16 13:31:36 +0300

    create new package

---
+++ src/com/git/test8/DistanceOfCities.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.git.test8;
+
+import java.util.List;
+
+/**
+ *
+ * @author summe
+ */
+public class DistanceOfCities {
+
+    public static Integer chooseBestSum(int t, int k, List<Integer> ls) {
+        // your code
+        return 1;
+    }
+}
